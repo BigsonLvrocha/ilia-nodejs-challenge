ad65ba22a9cbcb0b52b62b5a35cc3f7da305fb18
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-18 17:46:02 -0300

    style: apply missing styling

--- services/transactions/src/index.spec.ts
+++ services/transactions/src/index.spec.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect } from '@jest/globals';
 
-describe("index", () => {
-  it("should run", () => {
+describe('index', () => {
+  it('should run', () => {
     expect(true).toBe(true);
   });
 });
