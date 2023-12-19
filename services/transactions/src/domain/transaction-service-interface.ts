9efa595a7cea338213cd8e82e14f2c4b27e37ee6
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-18 21:55:07 -0300

    feat: add find to methods

--- services/transactions/src/domain/transaction-service-interface.ts
+++ services/transactions/src/domain/transaction-service-interface.ts
@@ -1,5 +1,12 @@
+import { type TransactionType } from './transaction-type.enum.js';
 import { type Transaction } from './transactions.js';
+
+export interface TransactionFindOptions {
+  userId: string;
+  type?: TransactionType;
+}
 
 export interface TransactionServiceInterface {
   create: (transaction: Transaction) => Promise<void>;
+  find: (options: TransactionFindOptions) => Promise<Transaction[]>;
 }
