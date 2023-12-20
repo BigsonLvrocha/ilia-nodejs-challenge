61d04576f1f0ebaefcd34802b1771da409047363
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-20 00:00:55 -0300

    feat: implement balance controller

--- services/transactions/src/interfaces/public-api/transactions-public-api.module.ts
+++ services/transactions/src/interfaces/public-api/transactions-public-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionsModule } from '../../transactions.module.js';
 import { TransactionsController } from './transactions-controller.js';
+import { BalanceController } from './balance-controller.js';
 
 @Module({
   imports: [TransactionsModule],
-  controllers: [TransactionsController],
+  controllers: [TransactionsController, BalanceController],
 })
 export class TransactionsPublicApiModule {}
