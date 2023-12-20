8b5ef3605c61cf60bc4ce76fb7f07205a41aa0dd
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-20 10:25:51 -0300

    feat: implement user auth

---
+++ services/users/src/use-cases/auth-user-use-case.spec.ts
@@ -0,0 +1,80 @@
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
+import { User } from '../domain/user.js';
+import { Test } from '@nestjs/testing';
+import { providersEnum } from '../providers.enum.js';
+import { UserAuthFailedException } from '../domain/error/user-auth-failed-exception.js';
+import { AuthUserUseCase } from './auth-user-use-case.js';
+
+describe('AuthUserUseCase', () => {
+  const userRepositoryMockFactory = (): {
+    findByEmail: jest.Mock<() => Promise<User | null>>;
+  } => ({
+    findByEmail: jest.fn(async () => null),
+  });
+
+  let userRepositoryMock: ReturnType<typeof userRepositoryMockFactory>;
+  let useCase: AuthUserUseCase;
+
+  beforeEach(async () => {
+    const testModule = await Test.createTestingModule({
+      providers: [
+        AuthUserUseCase,
+        {
+          provide: providersEnum.UserRepository,
+          useFactory: userRepositoryMockFactory,
+        },
+      ],
+    }).compile();
+
+    userRepositoryMock = testModule.get(providersEnum.UserRepository);
+    useCase = testModule.get(AuthUserUseCase);
+  });
+
+  it('throws error when user is not found', async () => {
+    await expect(
+      useCase.execute({
+        email: 'john@gmail.com',
+        password: 'password',
+      })
+    ).rejects.toThrowError(UserAuthFailedException);
+
+    expect(userRepositoryMock.findByEmail).toHaveBeenCalledTimes(1);
+    expect(userRepositoryMock.findByEmail).toHaveBeenCalledWith(
+      'john@gmail.com'
+    );
+  });
+
+  it('throws error when password is incorrect', async () => {
+    const user = await User.createNewUser({
+      email: 'john@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'password',
+    });
+
+    userRepositoryMock.findByEmail.mockResolvedValueOnce(user);
+
+    await expect(
+      useCase.execute({
+        email: 'john@gmail.com',
+        password: 'password2',
+      })
+    ).rejects.toThrowError(UserAuthFailedException);
+  });
+
+  it('returns user when auth is successful', async () => {
+    const user = await User.createNewUser({
+      email: 'john@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'password',
+    });
+
+    userRepositoryMock.findByEmail.mockResolvedValueOnce(user);
+
+    await useCase.execute({
+      email: 'john@gmail.com',
+      password: 'password',
+    });
+  });
+});
