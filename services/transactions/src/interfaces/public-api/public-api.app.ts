9d7fb62870227d21d073ab940c8124ea6838e41d
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-20 00:25:08 -0300

    feat: implement public api app creation

---
+++ services/transactions/src/interfaces/public-api/public-api.app.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { PublicApiModule } from './public-api.module.js';
+import { type INestApplication, ValidationPipe } from '@nestjs/common';
+
+export async function startPublicApiApp(): Promise<INestApplication> {
+  const app = await NestFactory.create(PublicApiModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3001);
+  return app;
+}
