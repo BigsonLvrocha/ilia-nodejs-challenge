2acf47a3fca3027a0cfce94c126daa6b768b5438
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-20 01:55:35 -0300

    feat: implement user listing

--- services/users/src/domain/user-repository-interface.ts
+++ services/users/src/domain/user-repository-interface.ts
@@ -2,4 +2,5 @@ import { type User } from './user.js';
 
 export interface UserRepositoryInterface {
   create: (user: User) => Promise<void>;
+  findAll: () => Promise<User[]>;
 }
--- services/users/src/infrastructure/mongoose-user-repository.ts
+++ services/users/src/infrastructure/mongoose-user-repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { type UserRepositoryInterface } from '../domain/user-repository-interface.js';
-import { type User } from '../domain/user.js';
+import { User } from '../domain/user.js';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDefinition, UserModel } from './model/user.schema.js';
 
@@ -17,6 +17,19 @@ export class MongooseUserRepository implements UserRepositoryInterface {
       lastName: user.lastName,
       email: user.email,
       passwordHash: user.passwordHash,
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await this.userModel.find().exec();
+    return users.map((user) => {
+      return new User({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        passwordHash: user.passwordHash,
+      });
     });
   }
 }
