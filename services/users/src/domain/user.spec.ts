ff53695f2a15f6476a297d83ac4c47f6b0d683a7
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2023-12-20 01:06:59 -0300

    test: complete user with test

--- services/users/src/domain/user.spec.ts
+++ services/users/src/domain/user.spec.ts
@@ -18,4 +18,60 @@ describe('User', () => {
     expect(user.passwordHash).not.toEqual('123');
     expect(await bcrypt.compare('123', user.passwordHash)).toBe(true);
   });
+
+  it('changes the first name', async () => {
+    const user = new User({
+      email: 'john.cage@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      passwordHash: '123',
+      id: '123',
+    });
+
+    user.changeFirstName('Johnny');
+
+    expect(user.firstName).toEqual('Johnny');
+  });
+
+  it('changes the last name', async () => {
+    const user = new User({
+      email: 'john.cage@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      passwordHash: '123',
+      id: '123',
+    });
+
+    user.changeLastName('Does');
+
+    expect(user.lastName).toEqual('Does');
+  });
+
+  it('changes the email', async () => {
+    const user = new User({
+      email: 'john.cage@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      passwordHash: '123',
+      id: '123',
+    });
+
+    user.changeEmail('john.travolta@gmail.com');
+
+    expect(user.email).toEqual('john.travolta@gmail.com');
+  });
+
+  it('changes the password', async () => {
+    const user = new User({
+      email: 'john.cage@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      passwordHash: '123',
+      id: '123',
+    });
+
+    await user.changePassword('1234');
+
+    expect(await bcrypt.compare('1234', user.passwordHash)).toBe(true);
+  });
 });
